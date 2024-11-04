065ebd47afbc4c332e28388505c6af83bf44b863
Artem Puzenko <a.puzenko2004@gmail.com>
2024-11-04 21:01:09 +0200

    make build

---
+++ index.202fba7e.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#largeImg");document.querySelector("#thumbs").addEventListener("click",function(t){t.preventDefault(),("IMG"===t.target.tagName||"A"===t.target.tagName)&&(e.src=t.target.closest("a").href)});
+//# sourceMappingURL=index.202fba7e.js.map
