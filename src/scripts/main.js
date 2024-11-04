092a57527660f816f3c957bc384574c0a770dcb3
Artem Puzenko <a.puzenko2004@gmail.com>
2024-11-04 20:46:54 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const currentImg = document.querySelector('#largeImg');
+const thumbs = document.querySelector('#thumbs');
+
+// eslint-disable-next-line no-shadow
+thumbs.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  if (event.target.tagName === 'IMG' || event.target.tagName === 'A') {
+    currentImg.src = event.target.closest('a').href;
+  }
+});
